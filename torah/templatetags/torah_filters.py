4bd46c570206e6b61db46729fe997606c639a873
suhailvs <suhailvs@gmail.com>
2018-12-29 09:56:03 +0530

    replace fie with yvH

--- torah/templatetags/torah_filters.py
+++ torah/templatetags/torah_filters.py
@@ -27,6 +27,13 @@ def get_hebrewletter(paleoletter):
     HEBREW_UNICODE = ['\u05d0','\u05d1','\u05d2','\u05d3','\u05d4','\u05d5','\u05d6','\u05d7','\u05d8','\u05d9','\u05db','\u05dc','\u05de','\u05e0','\u05e1','\u05e2','\u05e4','\u05e6','\u05e7','\u05e8','\u05e9','\u05ea']
     return HEBREW_UNICODE[PATTERN.index(paleoletter)]
 
+@register.filter(name='replace_fie')
+def replace_fie(paleoword):
+    """
+    Replace f -> v, i -> y, e -> H
+    """
+    return paleoword.replace('f','v').replace('i','y').replace('e','H')
+
 
 from torah.models import Word
 
